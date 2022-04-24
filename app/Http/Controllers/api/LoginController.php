f32392759bfe1ae7f00cffd9c2469db42821f615
ktmsulaim <ktmsulaim@gmail.com>
2022-04-24 17:58:24 +0530

    Basic login implemented

--- app/Http/Controllers/api/LoginController.php
+++ app/Http/Controllers/api/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -11,7 +13,31 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         try {
-            return response()->json($request->all());
+            $user = User::where('email', $request->email)->first();
+
+            if(!$user) {
+                return response()->json([
+                    'success' => false,
+                    'data' => 'User not found'
+                ], 404);
+            }
+
+            if(!Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'success' => false,
+                    'data' => 'Invalid password'
+                ], 401);
+            }
+
+            $token = $user->createToken('Donation')->plainTextToken;
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'token' => $token,
+                    'user' => $user
+                ]
+            ], 200);
         } catch (\Throwable $th) {
             throw $th;
         }
